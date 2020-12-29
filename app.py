0f46cda59b6bb468ae92d85a6b15a93ca033e978
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-12-30 04:41:18 +0700

    add rate

--- app.py
+++ app.py
@@ -6,14 +6,23 @@ from colored import fg, bg, attr
 
 print ('%s%sTimedoctor CLI%s' % (fg(231),bg(12), attr(0)))
 
+RATE_PER_HOUR = 2.5
+
 timedoctor = Client(
     email = TIMEDOCTOR_EMAIL,
     password = TIMEDOCTOR_PASSWORD
 ) 
 
+# Getting Hours
 timedoctor.login()
 data = timedoctor.get_time()
 total = timedoctor.parse_summary(data)['total']
 
-print ('You worked for ' + '%s%s%s%s' % (fg(231),bg(4),total, attr(0)))
-# print ('You should be paid with ' + '%s%s$62%s' % (fg(231),bg(2), attr(0)))
+# Getting Rate
+total_worked_hours = int(total.split('h')[0])
+salary = RATE_PER_HOUR * total_worked_hours
+
+
+
+print ('You worked for ' + '%s%s%s%s' % (fg(231),bg(4),total, attr(0)) + ' this week')
+print ('You should be paid ' + '%s%s$%s%s' % (fg(231),bg(2),salary, attr(0)) + ' from $2.5/hr')
