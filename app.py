98962afa561a8b63118ccfa2d997e71f06dc0aac
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-12-30 04:23:03 +0700

    add interface

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from timedoctor.api import Client
+from timedoctor.utils import convert_timestamp_to_hour
 from settings import TIMEDOCTOR_EMAIL, TIMEDOCTOR_PASSWORD
+
+from colored import fg, bg, attr
+
+print ('%s%sTimedoctor CLI%s' % (fg(231),bg(12), attr(0)))
 
 timedoctor = Client(
     email = TIMEDOCTOR_EMAIL,
@@ -8,3 +13,7 @@ timedoctor = Client(
 
 timedoctor.login()
 data = timedoctor.get_time()
+total = timedoctor.parse_summary(data)['total']
+
+print ('You worked for ' + '%s%s%s%s' % (fg(231),bg(4),total, attr(0)))
+# print ('You should be paid with ' + '%s%s$62%s' % (fg(231),bg(2), attr(0)))
